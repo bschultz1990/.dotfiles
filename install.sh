0113486848004f0fc67b2097779fbf8fdf622cd7
Ben S <bschultz1990@users.noreply.github.com>
2022-12-31 16:11:11 -0600

    Added github cli install

--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@
 # update system
 sudo apt update && sudo apt upgrade -y
 
+# install gh cli
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+echo "GitHub CLi installed. If this failed, visit https://github.com/cli/cli/blob/trunk/docs/install_linux.md for options."
+
 # General dependencies
 sudo apt install nala -y
 sudo nala install git -y
