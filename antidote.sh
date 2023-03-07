ea9693bba1f3b98c7af49efedc6bdcbe98ed1bbf
Will <will@noemail.com>
2023-03-07 15:35:41 -0600

    Added files via CMD

--- antidote.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing Powerlevel10k..."
-antidote install "romkatv/powerlevel10k"
-
-echo "Powerlevel10k installed! Restarting shell..."
-echo "'p10k csnfigure' to configure."
-
