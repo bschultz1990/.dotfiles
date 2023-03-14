451a6c7187bda29142a87030eb627fa49c18b1e7
Will <will@noemail.com>
2023-03-13 21:30:07 -0500

    further massaged pkginstall. Now it checks for apps in /Applications if Brew installs a cask.

--- install/functions.sh
+++ install/functions.sh
@@ -53,45 +53,23 @@ done
 }
 
 function pkginstall {
+  echo ""
   # Exit if 'jq' isn't installed
   if ! which "jq" &> /dev/null; then
     echo "FATAL: 'jq' is not installed. Please install before running this script again."
     exit
   fi
   package=$(jq -r ".[\"$1\"].\"$option\"" packages.json)
-  if ! type "$1" &> /dev/null; then 
-    echo "Installing package: " "$package..."
-    if [ "$2" == "--cask" ] && [ "$system" == "Darwin" ]; then
-      echo "Cask on Mac!"
-      ${install_command[@]} $2 "$package" ${suffix[@]}
-    else
-      ${install_command[@]} "$package" ${suffix[@]}
-    fi
+  app=$(jq -r ".[\"$1\"].app" packages.json)
+  [ "$app" != null ] && echo "Checking for" "$app" "in /Applications first..."
+  if command -v "$1" >/dev/null 2>&1 || [ -e "/Applications/""$app" ]; then
+    echo "$1 is already installed. Skipping install of $package"
   else
-    echo "$1" "is already installed. Skipping install of" "$package"
+    echo "Installing package: $package ..."
+    [ "$2" == "--cask" ] && [ "$system" == "Darwin" ] && install_command+=( "$2" )
+    ${install_command[@]} "$package" "${suffix[@]}"
   fi
 }
-
-# function pkginstall {
-#   # Warn user and exit if 'jq' is not installed.
-#   if ! which "jq" &> /dev/null; then
-#     echo "FATAL: 'jq' is not installed. Please install before running this script again."
-#     exit
-#   else
-#     package=$(jq -r ".[\"$1\"].\"$option\"" packages.json)
-#     if ! type "$1" &> /dev/null; then 
-#       echo "Installing package: " "$package..."
-#       if [ "$2" == "--cask" ] && [ "$system" == "Darwin" ]; then
-#         echo "Cask on Mac!"
-#         ${install_command[@]} $2 "$package" ${suffix[@]}
-#       else
-#         ${install_command[@]} "$package" ${suffix[@]}
-#       fi
-#     else
-#       echo "$1" "is already installed. Skipping install of" "$package"
-#     fi
-#   fi
-# }
 
 # Create the Apps directory if it doesn't exist already.
 function set_appdir {
