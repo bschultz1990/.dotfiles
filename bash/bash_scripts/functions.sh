d9ae2e1a96eeee1824343f2bcb2cf86914ec0836
Ben S <bens@noemail.com>
2024-06-08 16:31:21 -0500

    More finagling

--- bash/bash_scripts/functions.sh
+++ bash/bash_scripts/functions.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+paths()
+{
+  fzf < <(cat ~/.dotfiles/bash/bash_scripts/paths.sh)
+}
+
+paths_edit()
+{
+  nvim ~/.dotfiles/bash/bash_scripts/paths.sh
+}
+
+# load neovim sessions
 sessions()
 {
   c ~/Documents/vim_sessions/
-  nvim -S $(fzf)
+  nvim -S "$(fzf)"
 }
 
 update()
@@ -16,6 +27,7 @@ update()
 	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 	chmod u+x nvim.appimage
 	sudo mv nvim.appimage /usr/local/bin/nvim
+  nvim --version
 }
 
 c() {
--- bash/bash_scripts/paths.sh
+++ bash/bash_scripts/paths.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 alias bashconfig="c ~/.dotfiles/bash/bash_scripts"
 alias nvimconfig="c ~/.config/nvim/lua/"
+alias docs="c ~/Documents/"
