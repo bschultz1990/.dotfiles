143a33737664dbb4a88f63c7fed686a42b0fdf7d
Ben S. <bschultz1990@gmail.com>
2025-07-30 19:29:36 -0500

    Edit install_fedora

--- bash/install_fedora.sh
+++ bash/install_fedora.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
 sudo dnf update -y
-dnf upgrade -y
-dnf install neovim fzf glab fastfetch
+sudo dnf upgrade -y
+sudo dnf remove *libreoffice* -y
+
+echo "Installing mbpfan"
+sudo dnf install mbpfan -y
+
+sudo echo"
+
+min_fan1_speed = 4000
+min_fan2_speed = 4000
+max_fan1_speed = 6200
+max_fan2_speed = 6200
+" >> /etc/mbpfan.conf
+
+sudo systemctl start mbpfan.service
+sudo systemctl enable mbpfan
+sudo systemctl enable systemd-resolved
+
+echo "Installing wireless drivers. Reboot to complete the install."
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install broadcom-wl -y
+
+echo "Installing extras..."
+sudo dnf install neovim fzf glab konsole gh glab fastfetch -y
 
 echo "Installing .dotfiles and updating ~/.bashrc..."
 gh auth login
@@ -17,13 +39,11 @@ if [ -d ~/.dotfiles/bash/bash_scripts/ ]; then
     done
 fi" >> ~/.bashrc
 
-echo "Installing wireless drivers. Reboot to complete the install."
-
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install broadcom-wl
 
 echo "Getting notes..."
 cd ~/Documents/
 glab auth login
 ghab repo clone notes
 cd ~/
+
+echo "All done! :) Restarting soon..."
