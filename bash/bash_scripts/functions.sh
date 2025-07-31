bd0824cf02c554413cd93f5a56cfe05594db12dc
Ben S. <bschultz1990@gmail.com>
2025-07-31 13:24:50 -0500

    Updated update() for dnf

--- bash/bash_scripts/functions.sh
+++ bash/bash_scripts/functions.sh
@@ -2,18 +2,19 @@
 
 update()
 {
-	sudo apt update
-	sudo apt upgrade -y
-  sudo apt autoremove
+	sudo dnf update -y
+	sudo dnf upgrade -y
+  sudo dnf clean all -y
+  sudo dnf autoremove -y
   flatpak update -y
 
-  readonly nvim_filename='nvim-linux-x86_64.appimage'
+  # readonly nvim_filename='nvim-linux-x86_64.appimage'
 
 	# Update NeoVim
-	curl -LO https://github.com/neovim/neovim/releases/latest/download/$nvim_filename
-	chmod u+x $nvim_filename
-	sudo mv $nvim_filename /usr/local/bin/nvim
-  nvim --version
+	# curl -LO https://github.com/neovim/neovim/releases/latest/download/$nvim_filename
+	# chmod u+x $nvim_filename
+	# sudo mv $nvim_filename /usr/local/bin/nvim
+  # nvim --version
 }
 
 c() {
