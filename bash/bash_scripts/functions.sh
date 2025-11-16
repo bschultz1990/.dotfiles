6556be538f3ce1f5d484a5a89ae4f8b25a48ff24
Ben <bschultz1990@gmail.com>
2025-11-16 12:58:01 -0600

    Update the update command.

--- bash/bash_scripts/functions.sh
+++ bash/bash_scripts/functions.sh
@@ -2,20 +2,26 @@
 
 update()
 {
-	sudo pamac update
-	sudo pamac upgrade
-  sudo pamac clean all
-  # sudo pamac autoremove
-  flatpak update
-
-  # readonly nvim_filename='nvim-linux-x86_64.appimage'
-
-	# Update Neovim
-	# curl -LO https://github.com/neovim/neovim/releases/latest/download/$nvim_filename
-	# chmod u+x $nvim_filename
-	# sudo mv $nvim_filename /usr/local/bin/nvim
-  # nvim --version
+  echo "Updating system..."
+	pamac update --no-confirm
+	pamac upgrade --no-confirm
+  echo "Cleaning local packages..."
+  pamac clean all
+  pamac remove -o
+  echo "Cleaning flatpak packages..."
+  flatpak update --noninteractive
+  flatpak uninstall --unused --noninteractive
 }
+
+# update_neovim() {
+#   readonly nvim_filename='nvim-linux-x86_64.appimage'
+#
+# 	Update Neovim
+# 	curl -LO https://github.com/neovim/neovim/releases/latest/download/$nvim_filename
+# 	chmod u+x $nvim_filename
+# 	sudo mv $nvim_filename /usr/local/bin/nvim
+#   nvim --version
+# }
 
 c() {
   clear
