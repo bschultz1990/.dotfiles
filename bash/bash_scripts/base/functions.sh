03e0da2567f42ea88018eeca8883ae330785b15c
Ben S. <bschultz1990@gmail.com>
2026-01-24 09:49:11 -0600

    Add directory traversal functions

--- bash/bash_scripts/base/functions.sh
+++ bash/bash_scripts/base/functions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/requirements.sh"
+
 c() {
   clear
   cd "$1" || cd . || return
@@ -11,22 +15,32 @@ b() {
   ls -a1
 }
 
-f() {
+f() { 
+  if ! requirements "fzf"; then return; fi
+  open "$(fzf)"; 
+}
+
+n() {
+  if ! requirements "nvim" "fzf"; then return; fi
   nvim "$(fzf)"
+}
+
+d() { 
+  if ! requirements "fdfind"; then return; fi
+  cd "$(fdfind -type d | fzf)" || return
 }
 
 update_neovim() {
   nvim_filename='nvim-linux-x86_64.appimage'
 
-	curl -LO https://github.com/neovim/neovim/releases/latest/download/$nvim_filename
-	chmod u+x $nvim_filename
-	sudo mv $nvim_filename /usr/local/bin/nvim
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/$nvim_filename
+  chmod u+x $nvim_filename
+  sudo mv $nvim_filename /usr/local/bin/nvim
   nvim --version
 }
 
 notes() {
-  if ! requirements "glab" "nvim"; then
-    return
+  if ! requirements "glab" "nvim"; then return
   else
     if ! [ -d ~/Documents/notes/ ]; then
       cd ~/Documents/ || return
