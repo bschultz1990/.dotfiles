08b3da4cff794842dc2067056469f5b375a52e9f
Ben S. <bschultz1990@users.noreply.github.com>
2026-01-25 21:10:19 -0600

    Split installation of git, curl, and wget into separate commands

--- install/install_ubuntu_prerequisites.sh
+++ install/install_ubuntu_prerequisites.sh
@@ -2,7 +2,9 @@
 
 sudo apt install macfanctld -y
 sudo apt install broadcom-sta-dkms -y
-sudo apt install git curl wget -y
+sudo apt install git -y
+sudo apt install curl -y
+sudo apt install wget -y
 sudo apt install gh -y
 sudo apt install glab -y
 gh auth login
