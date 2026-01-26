f938b90338835b7e76b3670a26afc7fa4a0dc7bd
Ben S. <bschultz1990@users.noreply.github.com>
2026-01-25 20:43:53 -0600

    Fix font download URL and update installation command

--- install/install_ubuntu.sh
+++ install/install_ubuntu.sh
@@ -21,10 +21,10 @@ sudo apt install font-manager
 cd "$HOME/Downloads" || return
 font_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
 font_zip_name="${font_url##*/}"
-wget "$font_zip_name"
+wget "$font_url"
 echo "Installing $font_zip_name and refreshing font cache..."
 
-font-manager -i 
+font-manager -i "$font_zip_name"
 fc-cache -fv
 
 
