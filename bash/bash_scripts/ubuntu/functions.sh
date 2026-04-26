fc1e06562b5a35d9df2ed31ec6150cca1d73b613
Ben Schultz <bschultz1990@gmail.com>
2026-04-25 20:32:29 -0400

    Add lazygit install to bash/ubuntu.sh

--- bash/bash_scripts/ubuntu/functions.sh
+++ bash/bash_scripts/ubuntu/functions.sh
@@ -15,7 +15,19 @@ update() {
   downloadGitHubAppImage neovim/neovim 3 nvim
   downloadGitHubAppImage neovide/neovide 3 neovide
   downloadGitHubAppImage pkgforge-dev/ghostty-appimage 2 ghostty
+  downloadGitHubAppImage pkgforge-dev/ghostty-appimage 2 ghostty
   bash <(curl -sL https://git.io/go-installer)
+  downloadGitHubAppImage TibixDev/winboat 2 winboat
+
+  ## Lazygit start------
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  LAZYGIT_ARCH=$(uname -m | sed -e 's/aarch64/arm64/')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_${LAZYGIT_ARCH}.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit -D -t /usr/local/bin/
+  rm lazygit.tar.gz
+  rm lazygit
+  ## Lazygit end--------
 
   flatpak update -y
 
