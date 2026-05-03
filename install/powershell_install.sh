0e0a9a9360c89584d900881688c9b600da498b6c
Ben Schultz <bschultz1990@gmail.com>
2026-05-03 17:19:57 -0400

    Update powershell_install.sh to latest stable version of PowerShell 7.

--- install/powershell_install.sh
+++ install/powershell_install.sh
@@ -7,17 +7,17 @@ sudo apt-get update
 sudo apt-get install -y wget
 
 # Download the PowerShell package file
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/powershell_7.4.2-1.deb_amd64.deb
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.6.1/powershell_7.6.1-1.deb_amd64.deb
 
 ###################################
 # Install the PowerShell package
-sudo dpkg -i powershell_7.4.2-1.deb_amd64.deb
+sudo dpkg -i powershell_7.6.1-1.deb_amd64.deb
 
 # Resolve missing dependencies and finish the install (if necessary)
 sudo apt-get install -f
 
 # Delete the downloaded package file
-rm powershell_7.4.2-1.deb_amd64.deb
+rm powershell_7.6.1-1.deb_amd64.deb
 
 # Start PowerShell
 pwsh
