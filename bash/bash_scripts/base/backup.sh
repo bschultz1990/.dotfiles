04ab725f093d3b38bedca83fc13474e4134d69c5
Ben Schultz <bschultz1990@gmail.com>
2026-06-30 20:34:45 -0500

    replace 'b2' with 'backup.sh'

--- bash/bash_scripts/base/backup.sh
+++ bash/bash_scripts/base/backup.sh
@@ -2,6 +2,7 @@
 backup() {
   if ! requirements "b2"; then return; fi
   directory="/mnt/data/Files/"
+  bucket="JediArchiveBackup/FalconII/"
   cd "$directory" || echo "Error: $directory does not exist"
-  b2 sync --keep-days 30 --replace-newer . b2:JediArchiveBackup/FalconII/
+  b2 sync --keep-days 30 --replace-newer . b2:$bucket
 }
