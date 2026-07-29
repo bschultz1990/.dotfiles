3e8e40dd94aa8e4e116728b50b42d6b99299221f
Ben Schultz <bschultz1990@gmail.com>
2026-07-28 21:07:47 -0500

    Add 'syncnotes.sh'

--- bash/bash_scripts/base_utilities/syncnotes.sh
+++ bash/bash_scripts/base_utilities/syncnotes.sh
@@ -15,8 +15,17 @@ syncnotes()
 
   if [ -d "$destPath/$destFolder" ]; then
     gio trash "$destPath/$destFolder"
-    echo "Moved '$destPath/$destFolder' to the trash."
+    echo "Moved old '$destPath/$destFolder' to the trash."
   fi
 
-  unzip "$(realpath "$watchPath/$watchFile")" -d "$destPath"
+  unzip -q "$(realpath "$watchPath/$watchFile")" -d "$destPath"
+
+  # Cleanup
+  gio trash "$watchPath/$watchFile"
+  gio trash "$destPath/__MACOSX"
+  cd "$destPath/$destFolder" ||
+    { 
+      echo "'$destPath/$destFolder' does not exist." && return
+    }
+  git status
 }
